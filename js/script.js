9e50491087332bf13e2f991cd9b78834cbf31248
Victor friboulet <victorfriboulet07@gmail.com>
2023-10-02 15:33:38 +0200

    :bug: correction et adaptation à weather card

--- js/script.js
+++ js/script.js
@@ -4,45 +4,47 @@ let fond = document.getElementById("fond")
 let recherche = document.getElementById("recherche")
 
 recherche.addEventListener("click", ()=>{
-    let typetemps = WeatherCard.weather()/*methode Victor*/
-    if(typetemps = 'soleil'){
+    let typetemps = weather.determineWeather()/*methode Victor*/
+    //console.log(typetemps);
+    if(typetemps == 'soleil'){
         fond.style.background = 'url("../image/theme_clair.jpg") no-repeat fixed'
         fond.style.backgroundSize = 'cover'
         titre.classList.add('sombre')
     }
-    if(typetemps = 'neige'){
+    else if(typetemps == 'neige'){
         fond.style.background = 'url("../image/theme_neigeux.jpg") no-repeat fixed'
         fond.style.backgroundSize = 'cover' 
         titre.classList.add('sombre')
     }
-    if(typetemps = 'nuage'){
+    else if(typetemps == 'nuage'){
         fond.style.background = 'url("../image/theme_nuageux.jpg") no-repeat fixed'
         fond.style.backgroundSize = 'cover'
         titre.classList.add('sombre')
     }
-    if(typetemps = 'pluie'){
+    else if(typetemps == 'pluie'){
         fond.style.background = 'url("../image/theme_pluie.jpg") no-repeat fixed'
         fond.style.backgroundSize = 'cover'
         titre.classList.add('sombre')
     }
-    if(typetemps = 'vent'){
+    else if(typetemps == 'vent'){
         fond.style.background = 'url("../image/theme_vent.jpg") no-repeat fixed'
         fond.style.backgroundSize = 'cover'
         titre.classList.add('sombre')
     }
-    if(typetemps = 'brume'){
+    else if(typetemps == 'brume'){
         fond.style.background = 'url("../image/theme_brume.jpg") no-repeat fixed'
         fond.style.backgroundSize = 'cover'
         titre.classList.add('sombre')
     }
-    if(typetemps = 'orage'){
-        fond.style.background = 'url("../image/theme_orageux.jpg") no-repeat fixed'
-        fond.style.backgroundSize = 'cover'
-        titre.classList.add('sombre')
-    }
-    if(typetemps = 'grele'){
+    else if(typetemps == 'grele'){
         fond.style.background = 'url("../image/theme_grele.jpg") no-repeat fixed'
         fond.style.backgroundSize = 'cover'
         titre.classList.add('sombre')
     }
+    else if(typetemps == 'orage'){
+        fond.style.background = 'url("../image/theme_orageux.jpg") no-repeat fixed'
+        fond.style.backgroundSize = 'cover'
+        titre.classList.add('sombre')
+    }
+    
 })
