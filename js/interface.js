fb7c3e36f22ab055f2edcb81a304a4c43262df7a
Victor_Friboulet <victorfriboulet07@gmail.com>
2023-10-03 10:17:31 +0200

    merge conflict

--- js/interface.js
+++ js/interface.js
@@ -86,24 +86,14 @@ function onSelectionneVille(){
 function afficherMeteo(weatherCard){
     labelVille.textContent = weatherCard.Ville();
     
-<<<<<<< HEAD
     labelBref.textContent = " :" + weatherCard.meteo(0);
-=======
-    labelBref.textContent = "Temps :" + weatherCard.meteo();
->>>>>>> d4c2a5017a25d771f0ee3379c37cfa00204bee75
     
     labelTemperatureMin.textContent = `${weatherCard.TempMin(0)} °C`;
     labelTemperatureMax.textContent = `${weatherCard.TempMax(0)} °C`;
 
-<<<<<<< HEAD
     labelPluie.textContent = weatherCard.pourcentPluie(0) + "%";
     
     labelEnsoleillement.textContent =  `${weatherCard.ensoleillement(0)} heures`;
-=======
-    labelPluie.textContent = weatherCard.pourcentPluie() + "%";
-    
-    labelEnsoleillement.textContent =  `${weatherCard.ensoleillement()} heures`;
->>>>>>> d4c2a5017a25d771f0ee3379c37cfa00204bee75
 
     actualiserArrierePlan(weatherCard);
 
